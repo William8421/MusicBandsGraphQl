6456fc4cfe673a510a9bfaed01b9c5147557d98f
william <william.mallak1@gmail.com>
2023-01-17 17:37:41 +0100

    added dotenv

--- server/app.js
+++ server/app.js
@@ -3,16 +3,19 @@ const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 const cors = require('cors');
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 mongoose.set('strictQuery', false);
 
 const app = express();
+const PORT = process.env.PORT || 8002;
 
 app.use(cors());
 
-mongoose.connect(
-  'mongodb+srv://william:myPlayList@cluster0.d6vksrd.mongodb.net/my-play-list'
-);
+mongoose.connect(process.env.MONGO_URI);
+// mongoose.connect(
+//   'mongodb+srv://william:myPlayList@cluster0.d6vksrd.mongodb.net/my-play-list'
+// );
 mongoose.connection.once('open', () => {
   console.log('connected to database');
 });
